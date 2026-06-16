e297e0a643ff4dd3a59e20421078a82abbd8688a
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-06-16 18:58:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const button = document.getElementById("getWeather");
+const weatherDiv = document.getElementById("weatherData");
+
+button.addEventListener("click", async () => {
+	const apiKey = "b398dd1903101ab014af97c23715e3b6";
+	const city = "London";
+
+	try {
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+		);
+
+		const data = await response.json();
+
+		weatherDiv.textContent = `Current weather in London: ${data.weather[0].main}`;
+	} catch (error) {
+		weatherDiv.textContent = "Failed to fetch weather data";
+	}
+});
