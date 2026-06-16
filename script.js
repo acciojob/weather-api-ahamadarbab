db3250eddd6f851ed09c9b5c9d4d3bd962d4fd30
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-06-16 19:07:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ const button = document.getElementById("getWeather");
 const weatherDiv = document.getElementById("weatherData");
 
 button.addEventListener("click", async () => {
-	const apiKey = "b398dd1903101ab014af97c23715e3b6";
-	const city = "London";
-
+	
 	try {
 		const response = await fetch(
-			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+			"https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de"
 		);
 
 		const data = await response.json();
